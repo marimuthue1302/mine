4144fae3d6677627d4eb88e24f047eca409e5a95
marimuthue1302 <marimuthue1302@gmail.com>
2023-03-23 09:14:02 +0530

    Mine

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Great from './component/Great';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Great/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Great.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Great() {
+  return <h1>Welcome Kavin! 🖤🖤</h1>;
+}
+
+export default Great;
